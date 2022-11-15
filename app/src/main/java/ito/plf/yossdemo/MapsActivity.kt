1fb5090a9ff81012ea8f987739851a97421de693
Orlando Lucero <orlando.lucero.camacho@gmail.com>
2022-11-15 16:43:42 -0600

    Se agregaron rutas y se mejoró el código

--- app/src/main/java/ito/plf/yossdemo/MapsActivity.kt
+++ app/src/main/java/ito/plf/yossdemo/MapsActivity.kt
@@ -1,28 +1,23 @@
 package ito.plf.yossdemo
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.graphics.Color
-import android.location.Location
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.libraries.places.api.Places
 import com.google.gson.Gson
@@ -36,26 +31,45 @@ import okhttp3.Request
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    // Variables necesarias para crear la instancia de google maps
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var currentLocation: Location
-    private val permissionCode = 101
+    //private lateinit var currentLocation: Location
+    //private val permissionCode = 101
     //private val TAG = MapsActivity::class.java.simpleName
     private val REQUEST_LOCATION_PERMISSION = 1
-    // GeeksforGeeks coordinates
+    // Coordenadas para prueba
     private var originLatitude: Double = 17.077831563590298
     private var originLongitude: Double = -96.74437901266337
-
-    // Coordinates of a park nearby
     private var destinationLatitude: Double = 17.060590930692484
     private var destinationLongitude: Double = -96.72546242802322
+    // Coordenadas para la creación de las diferentes rutas
+    lateinit var initPoint: LatLng
+    lateinit var firsPoint: LatLng
+    lateinit var secondPoint: LatLng
+    lateinit var thirdPoint: LatLng
+    lateinit var fourthPoint: LatLng
+    lateinit var fifthPoint: LatLng
+    lateinit var endPoint: LatLng
+    //var ArrayList<LatLng> arr:
+    // Color variables
+    // Arreglo para almacenar todos los colores ubicados en values/colors.xml
+    private val arrayColor = arrayOf(
+        "purple_200",
+        "purple_500",
+        "purple_700",
+        "teal_200",
+        "teal_700",
+        "black",
+        "white",
+        "pink",
+        "orange",
+        "mint",
+        "yellow",
+    )
 
-
-    /*companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
-    }*/
-
+    // Variable para crear una instancia de otra clase (recuperar todas las ubicaciones en el archivo json de lugares)
     private val places: List<Place> by lazy {
         PlacesReader(this).read()
     }
@@ -66,6 +80,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (savedInstanceState != null) {
+
+        }
+
         val ai: ApplicationInfo = applicationContext.packageManager.getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
         val value = ai.metaData["com.google.android.geo.API_KEY"]
         val key = value.toString()
@@ -73,7 +91,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if (!Places.isInitialized()) {
             Places.initialize(applicationContext, key)
         }
-
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
@@ -85,12 +102,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync { googleMap ->
             //addMarkers(googleMap)
             addClusteredMarkers(googleMap)
-
             // Set custom info window adapter.
-            // googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+            googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
         }
 
-        getCurrentLocationUser()
+        //getCurrentLocationUser()
 
         val gd = findViewById<Button>(R.id.directions)
         gd.setOnClickListener{
@@ -98,68 +114,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 mMap = it
                 val originLocation = LatLng(originLatitude, originLongitude)
                 //mMap.addMarker(MarkerOptions().position(originLocation))
+
                 val destinationLocation = LatLng(destinationLatitude, destinationLongitude)
                 //mMap.addMarker(MarkerOptions().position(destinationLocation))
-                val urll = getDirectionURL(originLocation, destinationLocation, key)
-                GetDirection(urll).execute()
+                //val urll = getDirectionURL(originLocation, destinationLocation, key)
+                //GetDirection(urll).execute()
+                defineRoute01()
+                /*val p1 = getDirectionURL(initPoint, firsPoint, key)
+                GetDirection(p1).execute()
+                val p2 = getDirectionURL(firsPoint, secondPoint, key)
+                GetDirection(p2).execute()
+                val p3 = getDirectionURL(secondPoint, thirdPoint, key)
+                GetDirection(p3).execute()
+                val p4 = getDirectionURL(thirdPoint, fourthPoint, key)
+                GetDirection(p4).execute()
+                val p5 = getDirectionURL(fourthPoint, fifthPoint, key)
+                GetDirection(p5).execute()
+                val p6 = getDirectionURL(fifthPoint, endPoint, key)
+                GetDirection(p6).execute()*/
+                // ruta 1
+                val p0 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(p0).execute()
+
+                // ruta 2
+                defineRoute02()
+                val r2 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r2).execute()
+
+                // ruta 3
+                defineRoute03()
+                val r3 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r3).execute()
+
+                // ruta 10
+                defineRoute10()
+                val r10 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r10).execute()
+
+                defineRoute27()
+                val r27 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r27).execute()
+
+                defineRoute29()
+                val r29 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r29).execute()
+
+                defineRoute47()
+                val r47 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r47).execute()
+
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 14F))
             }
         }
     }
 
-    private fun getCurrentLocationUser() {
-        if(ActivityCompat.checkSelfPermission(
-                this, android.Manifest.permission.ACCESS_FINE_LOCATION)!=
-                    PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                    PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
-            return
-        }
-
-        val getLocation = fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-            location ->
-            if (location != null) {
-                currentLocation = location
-
-                Toast.makeText(applicationContext, currentLocation.latitude.toString()+" "+
-                currentLocation.longitude.toString(), Toast.LENGTH_LONG).show()
-
-                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-                val mapFragment = supportFragmentManager
-                    .findFragmentById(R.id.map) as SupportMapFragment
-                mapFragment.getMapAsync(this)
-
-                mapFragment.getMapAsync { googleMap ->
-                    addClusteredMarkers(googleMap)
-
-                    // Set custom info window adapter.
-                    // googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
-                }
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode) {
-            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
-                getCurrentLocationUser()
-            }
-        }
-    }
-
-
     private fun getDirectionURL(origin:LatLng, dest:LatLng, secret: String) : String{
         return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}" +
                 "&destination=${dest.latitude},${dest.longitude}" +
                 "&sensor=false" +
-                "&mode=driving" +
+                "&mode=transit" +
                 "&key=$secret"
     }
 
@@ -219,6 +232,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         override fun onPostExecute(result: List<List<LatLng>>) {
             val lineoption = PolylineOptions()
+
             for (i in result.indices){
                 lineoption.addAll(result[i])
                 lineoption.width(10f)
@@ -229,7 +243,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         // Add a marker in Oaxaca and move the camera
@@ -239,24 +252,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Zoom out to zoom level 10, animating with a duration of 2 seconds.
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15f), 2000, null)
 
-        val originLocation = LatLng(originLatitude, originLongitude)
-        //mMap.addMarker(MarkerOptions().position(originLocation))
-        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 18F))
         //setMapLongClick(mMap)
-        //setPoiClick(mMap)
-        //setMapStyle(mMap)
-        //enableMyLocation()
+        enableMyLocation()
     }
 
     // Checks that users have given permission
-    /*private fun isPermissionGranted() : Boolean {
+    private fun isPermissionGranted() : Boolean {
         return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }*/
+            this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
 
     // Checks if users have given their location and sets location enabled if so.
-    /*private fun enableMyLocation() {
+    private fun enableMyLocation() {
         if (isPermissionGranted()) {
             if (ActivityCompat.checkSelfPermission(
                     this,
@@ -284,12 +291,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 REQUEST_LOCATION_PERMISSION
             )
         }
-    }*/
+    }
 
     // Callback for the result from requesting permissions.
     // This method is invoked for every call on requestPermissions(android.app.Activity, String[],
     // int).
-    /*override fun onRequestPermissionsResult(
+    override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray) {
@@ -301,7 +308,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 enableMyLocation()
             }
         }
-    }*/
+    }
 
     /**
      * Adds markers to the map with clustering support.
@@ -328,6 +335,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         googleMap.setOnCameraIdleListener {
             clusterManager.onCameraIdle()
         }
+    }
+
+    fun defineRoute01() {
+        initPoint = LatLng(17.135256862093122, -96.78051826442118)
+        firsPoint = LatLng(17.078870343480773, -96.74295633414324)
+        secondPoint = LatLng(17.066771358213252, -96.7364238245297)
+        thirdPoint = LatLng(17.055757051774304, -96.73030965482059)
+        fourthPoint = LatLng(17.054710142014812, -96.71300923674707)
+        fifthPoint = LatLng(17.070369018218564, -96.71360897592642)
+        endPoint = LatLng(17.09452923524003, -96.70664722097415)
+    }
+
+    fun defineRoute02() {
+        initPoint = LatLng(17.1029627094166, -96.74622724928918)
+        endPoint = LatLng(17.081782591386094, -96.69656797897142)
+    }
+
+    fun defineRoute03() {
+        initPoint = LatLng(16.996928707520496, -96.75258144125172)
+        endPoint = LatLng(17.08279434217865, -96.72372154609354)
+    }
+
+    fun defineRoute10() {
+        initPoint = LatLng(17.103285701601795, -96.7351856977866)
+        endPoint = LatLng(17.05549299988845, -96.68130462824716)
+    }
+
+    fun defineRoute27() {
+        initPoint = LatLng(16.99787860973189, -96.75826847357983)
+        endPoint = LatLng(17.09393498938193, -96.69787315669852)
+    }
+
+    fun defineRoute29() {
+        initPoint = LatLng(17.13508596500535, -96.78071377286666)
+        endPoint = LatLng(117.036950641175952, -96.70726816347653)
+    }
+
+    fun defineRoute47() {
+        initPoint = LatLng(17.056536913872083, -96.75828620870179)
+        endPoint = LatLng(17.05549651768387, -96.68130361631837)
     }
 
 }
