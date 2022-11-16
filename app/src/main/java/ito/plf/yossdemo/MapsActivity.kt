00c797880e9544dfe582af9c699a6cc5df856236
Orlando Lucero <orlando.lucero.camacho@gmail.com>
2022-11-15 22:41:06 -0600

    Se agregaron comentarios describiendo cada función del código y ahora los plylines generados están en colores aleatorios

--- app/src/main/java/ito/plf/yossdemo/MapsActivity.kt
+++ app/src/main/java/ito/plf/yossdemo/MapsActivity.kt
@@ -28,6 +28,7 @@ import ito.plf.yossdemo.place.PlaceRenderer
 import ito.plf.yossdemo.place.PlacesReader
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import kotlin.random.Random
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -35,54 +36,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    //private lateinit var currentLocation: Location
-    //private val permissionCode = 101
-    //private val TAG = MapsActivity::class.java.simpleName
     private val REQUEST_LOCATION_PERMISSION = 1
-    // Coordenadas para prueba
-    private var originLatitude: Double = 17.077831563590298
-    private var originLongitude: Double = -96.74437901266337
-    private var destinationLatitude: Double = 17.060590930692484
-    private var destinationLongitude: Double = -96.72546242802322
     // Coordenadas para la creación de las diferentes rutas
     lateinit var initPoint: LatLng
-    lateinit var firsPoint: LatLng
-    lateinit var secondPoint: LatLng
-    lateinit var thirdPoint: LatLng
-    lateinit var fourthPoint: LatLng
-    lateinit var fifthPoint: LatLng
     lateinit var endPoint: LatLng
-    //var ArrayList<LatLng> arr:
-    // Color variables
     // Arreglo para almacenar todos los colores ubicados en values/colors.xml
     private val arrayColor = arrayOf(
-        "purple_200",
-        "purple_500",
-        "purple_700",
-        "teal_200",
-        "teal_700",
-        "black",
-        "white",
-        "pink",
-        "orange",
-        "mint",
-        "yellow",
+        Color.GREEN,
+        Color.BLUE,
+        Color.GRAY,
+        Color.BLACK,
+        Color.CYAN,
+        Color.MAGENTA,
+        Color.RED,
+        Color.YELLOW,
+        Color.DKGRAY,
+        Color.LTGRAY
     )
+    private var randomColor : Int = 1
 
     // Variable para crear una instancia de otra clase (recuperar todas las ubicaciones en el archivo json de lugares)
     private val places: List<Place> by lazy {
         PlacesReader(this).read()
     }
 
+    // Función principal de nuestro programa, se define al lanzar la aplicación
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        if (savedInstanceState != null) {
-
-        }
 
         val ai: ApplicationInfo = applicationContext.packageManager.getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
         val value = ai.metaData["com.google.android.geo.API_KEY"]
@@ -94,80 +77,62 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtiene el SupportMapFragment y notifica cuando el mapa esté listo para ser usado
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         mapFragment.getMapAsync { googleMap ->
-            //addMarkers(googleMap)
             addClusteredMarkers(googleMap)
-            // Set custom info window adapter.
+            // Definimos el InfoWindowAdapter personalizado
             googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
         }
-
-        //getCurrentLocationUser()
 
         val gd = findViewById<Button>(R.id.directions)
         gd.setOnClickListener{
             mapFragment.getMapAsync {
                 mMap = it
-                val originLocation = LatLng(originLatitude, originLongitude)
-                //mMap.addMarker(MarkerOptions().position(originLocation))
-
-                val destinationLocation = LatLng(destinationLatitude, destinationLongitude)
-                //mMap.addMarker(MarkerOptions().position(destinationLocation))
-                //val urll = getDirectionURL(originLocation, destinationLocation, key)
-                //GetDirection(urll).execute()
-                defineRoute01()
-                /*val p1 = getDirectionURL(initPoint, firsPoint, key)
-                GetDirection(p1).execute()
-                val p2 = getDirectionURL(firsPoint, secondPoint, key)
-                GetDirection(p2).execute()
-                val p3 = getDirectionURL(secondPoint, thirdPoint, key)
-                GetDirection(p3).execute()
-                val p4 = getDirectionURL(thirdPoint, fourthPoint, key)
-                GetDirection(p4).execute()
-                val p5 = getDirectionURL(fourthPoint, fifthPoint, key)
-                GetDirection(p5).execute()
-                val p6 = getDirectionURL(fifthPoint, endPoint, key)
-                GetDirection(p6).execute()*/
                 // ruta 1
+                defineRoute01()
                 val p0 = getDirectionURL(initPoint, endPoint, key)
                 GetDirection(p0).execute()
-
                 // ruta 2
                 defineRoute02()
                 val r2 = getDirectionURL(initPoint, endPoint, key)
                 GetDirection(r2).execute()
-
                 // ruta 3
                 defineRoute03()
                 val r3 = getDirectionURL(initPoint, endPoint, key)
                 GetDirection(r3).execute()
-
                 // ruta 10
                 defineRoute10()
                 val r10 = getDirectionURL(initPoint, endPoint, key)
                 GetDirection(r10).execute()
-
+                // ruta 27
                 defineRoute27()
                 val r27 = getDirectionURL(initPoint, endPoint, key)
                 GetDirection(r27).execute()
-
+                // ruta 29
                 defineRoute29()
                 val r29 = getDirectionURL(initPoint, endPoint, key)
                 GetDirection(r29).execute()
-
+                // ruta 47
                 defineRoute47()
                 val r47 = getDirectionURL(initPoint, endPoint, key)
                 GetDirection(r47).execute()
-
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 14F))
+                // ruta 62
+                defineRoute62()
+                val r62 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r62).execute()
+                // ruta 71
+                defineRoute71()
+                val r71 = getDirectionURL(initPoint, endPoint, key)
+                GetDirection(r71).execute()
             }
         }
     }
 
+    // Función para escribir apropiadamente la sentencia de consulta de direcciones en el servidor
     private fun getDirectionURL(origin:LatLng, dest:LatLng, secret: String) : String{
         return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}" +
                 "&destination=${dest.latitude},${dest.longitude}" +
@@ -176,6 +141,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 "&key=$secret"
     }
 
+    // Función para trazar los polylines (líneas que representan las rutas), se trata de obtener
+    // los puntos y cambios de dirección necesarios para trazar la ruta.
     fun decodePolyline(encoded: String): List<LatLng> {
         val poly = ArrayList<LatLng>()
         var index = 0
@@ -208,6 +175,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         return poly
     }
 
+    // Clase para obtener las direcciones necesarias para trazar una ruta entre dos puntos
     @SuppressLint("StaticFieldLeak")
     private inner class GetDirection(val url : String) : AsyncTask<Void, Void, List<List<LatLng>>>(){
         override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
@@ -230,13 +198,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             return result
         }
 
+        // En este método es dónde se declaran las instrucciones para graficar un polyline
         override fun onPostExecute(result: List<List<LatLng>>) {
             val lineoption = PolylineOptions()
 
+            randomColor = Random.nextInt(0,9)
+
             for (i in result.indices){
                 lineoption.addAll(result[i])
-                lineoption.width(10f)
-                lineoption.color(Color.GREEN)
+                lineoption.width(12f)
+                lineoption.color(arrayColor[randomColor])
                 lineoption.geodesic(true)
             }
             mMap.addPolyline(lineoption)
@@ -245,24 +216,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        // Add a marker in Oaxaca and move the camera
-        //val current = LatLng(currentLocation.latitude, currentLocation.longitude)
+        // Creamos una variable ubicada en el centro de la ciudad de Oaxaca
         val oaxaca = LatLng(17.060590930692484, -96.72546242802322)
+        // Movemos la cámara hacia la dirección de nuestra variable
         mMap.moveCamera(CameraUpdateFactory.newLatLng(oaxaca))
-        // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        // Le indicamos a la app que realice un zoom de 15 puntos en dos segundos
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15f), 2000, null)
 
-        //setMapLongClick(mMap)
+        // Habilitamos la captura de localización actual del usuario
         enableMyLocation()
+        //setMapLongClick(mMap)
     }
 
-    // Checks that users have given permission
+    // Función para verificar que el usuario haya concedido los permisos necesarios
     private fun isPermissionGranted() : Boolean {
         return ContextCompat.checkSelfPermission(
             this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 
-    // Checks if users have given their location and sets location enabled if so.
+    // Función para verificar que el usuario haya aceptado proporcionar su ubicación actual
+    @SuppressLint("MissingPermission")
     private fun enableMyLocation() {
         if (isPermissionGranted()) {
             if (ActivityCompat.checkSelfPermission(
@@ -293,16 +266,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    // Callback for the result from requesting permissions.
-    // This method is invoked for every call on requestPermissions(android.app.Activity, String[],
-    // int).
+    // Callback para el resultado de la consulta de permisos
+    // Este método es invocado para cada llamada en requestPermissions(android.app.Activity, String[], int)
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        // Check if location permissions are granted and if so enable the
-        // location data layer.
+        // Check if location permissions are granted and if so enable the location data layer.
         if (requestCode == REQUEST_LOCATION_PERMISSION) {
             if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
                 enableMyLocation()
@@ -310,11 +281,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    /**
-     * Adds markers to the map with clustering support.
-     */
+    // Agregar marcadores al mapa con soporte de clusters
     private fun addClusteredMarkers(googleMap: GoogleMap) {
-        // Create the ClusterManager class and set the custom renderer.
+        // Crea una clase ClusterManager y define el renderizador personalizado
         val clusterManager = ClusterManager<Place>(this, googleMap)
         clusterManager.renderer =
             PlaceRenderer(
@@ -323,58 +292,56 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 clusterManager
             )
 
-        // Set custom info window adapter
+        // Define un info window adapter personalizado
         clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
 
-        // Add the places to the ClusterManager.
+        // Agrega las ubicaciones (obtenidas del archivo JSON) al ClusterManager
         clusterManager.addItems(places)
         clusterManager.cluster()
 
-        // Set ClusterManager as the OnCameraIdleListener so that it
-        // can re-cluster when zooming in and out.
+        // Definimos el ClusterManager como el OnCameraIdListener, para que pueda ser
+        // re-clusteado cuando se haga zoom in y zoom out
         googleMap.setOnCameraIdleListener {
             clusterManager.onCameraIdle()
         }
     }
 
+    // Definimos las direcciones para todas las rutas que usaremos en el programa
     fun defineRoute01() {
         initPoint = LatLng(17.135256862093122, -96.78051826442118)
-        firsPoint = LatLng(17.078870343480773, -96.74295633414324)
-        secondPoint = LatLng(17.066771358213252, -96.7364238245297)
-        thirdPoint = LatLng(17.055757051774304, -96.73030965482059)
-        fourthPoint = LatLng(17.054710142014812, -96.71300923674707)
-        fifthPoint = LatLng(17.070369018218564, -96.71360897592642)
         endPoint = LatLng(17.09452923524003, -96.70664722097415)
     }
-
     fun defineRoute02() {
         initPoint = LatLng(17.1029627094166, -96.74622724928918)
         endPoint = LatLng(17.081782591386094, -96.69656797897142)
     }
-
     fun defineRoute03() {
         initPoint = LatLng(16.996928707520496, -96.75258144125172)
         endPoint = LatLng(17.08279434217865, -96.72372154609354)
     }
-
     fun defineRoute10() {
         initPoint = LatLng(17.103285701601795, -96.7351856977866)
         endPoint = LatLng(17.05549299988845, -96.68130462824716)
     }
-
     fun defineRoute27() {
         initPoint = LatLng(16.99787860973189, -96.75826847357983)
         endPoint = LatLng(17.09393498938193, -96.69787315669852)
     }
-
     fun defineRoute29() {
         initPoint = LatLng(17.13508596500535, -96.78071377286666)
-        endPoint = LatLng(117.036950641175952, -96.70726816347653)
+        endPoint = LatLng(17.036950641175952, -96.70726816347653)
     }
-
     fun defineRoute47() {
         initPoint = LatLng(17.056536913872083, -96.75828620870179)
         endPoint = LatLng(17.05549651768387, -96.68130361631837)
+    }
+    fun defineRoute62() {
+        initPoint = LatLng(17.100964699971502, -96.76073536539256)
+        endPoint = LatLng(17.068067735865863, -96.6889075848271)
+    }
+    fun defineRoute71() {
+        initPoint = LatLng(17.064434263538747, -96.76110420063237)
+        endPoint = LatLng(17.037706362863844, -96.69302667681531)
     }
 
 }
